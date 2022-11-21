f6108122c6be7162eab2b5efe8fd1b781bdb9439
shidayanti12 <115630364+shidayanti12@users.noreply.github.com>
2022-11-20 16:29:12 -0800

    Add files via upload

---
+++ FragmentHome.java
@@ -0,0 +1,24 @@
+package com.example.fragmentlayoutex;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.fragmentlayoutex.R;
+
+public class FragmentHome extends Fragment {
+    View view;
+    public FragmentHome () {
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.home_fragment,container,false);
+        return view;
+    }
+}
